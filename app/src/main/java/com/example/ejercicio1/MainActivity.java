efc10086e10da33fd4ecad6ce41e5ffcfdb666e2
seraf7 <zaske_4@hotmail.com>
2020-12-03 15:52:36 -0600

    Implementacion de Spinner de carreras

--- app/src/main/java/com/example/ejercicio1/MainActivity.java
+++ app/src/main/java/com/example/ejercicio1/MainActivity.java
@@ -3,27 +3,39 @@ package com.example.ejercicio1;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ejercicio1.dialog.DatePickerFragment;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     EditText etFecha;
+    TextView tvCarrera;
+    Spinner spCarreras;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Asociar edit text
-        etFecha = (EditText) findViewById(R.id.etFecha);
-        //Asociar un metodo listener
+        etFecha = findViewById(R.id.etFecha);
+        //Asociar un metodo listener a etFecha
         etFecha.setOnClickListener(this);
+        //Asociar un metodo listener a spCarreras
+        spCarreras = findViewById(R.id.spCarreras);
+        spCarreras.setOnItemSelectedListener(this);
+        tvCarrera = findViewById(R.id.tvCarrera);
     }
 
     @Override
@@ -48,5 +60,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });{
             nuevoFragment.show(getSupportFragmentManager(), "view");
         }
+    }
+
+    //Metodo para manejar seleccion del Spinner
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String[] carreras = getResources().getStringArray(R.array.carreras);
+        tvCarrera.setText(carreras[position]);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
